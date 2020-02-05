5f708e174f7d48659393cf2908ada98fc87353c3
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-04 19:27:42 -0800

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -8,3 +8,10 @@ import pygame
 import tkinter
 from tkinter import filedialog
 import math
+
+# canvas dimensions in 0.1 mm
+origin = [2000 , 2700]
+size   = [4000 , 3500]
+center = [origin[0] + size[0] / 2 , origin[1] + size[1]/2]
+
+print(center)
