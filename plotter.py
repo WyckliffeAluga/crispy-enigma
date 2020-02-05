ad32b88c789e415548d839021b1a68a9579b90f2
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-04 19:35:30 -0800

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -14,4 +14,34 @@ origin = [2000 , 2700]
 size   = [4000 , 3500]
 center = [origin[0] + size[0] / 2 , origin[1] + size[1]/2]
 
-print(center)
+# maximum pixels allowed per side
+max_size   = 64
+cmd_unkown = 0
+cmd_ok     = 1
+cmd_console= 2
+
+pen_up  = 0
+pen_down= 1
+
+dir_cw  = 0
+dir_ccw = 1
+
+shaded_scalar_image     = 0
+crosshatch_scalar_image = 1
+svg_vector_image        = 2
+
+os_windows = 0
+os_raspbian= 1
+
+# set current os to windows
+current_os = os_windows
+
+# make helper functions
+
+def round_it(x):
+    """ Returns rounded interger """
+    return int(round(x))
+
+def remove_return(str):
+    """ Returns a string without the return clause"""
+    return str.rstrip('\r\n')
