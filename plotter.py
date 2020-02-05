9b37678bb84ac7af66b08b6ed1b1c0535fd89d50
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-04 19:20:40 -0800

    iniitated plotter

--- plotter.py
+++ plotter.py
@@ -1,12 +1,10 @@
 
+import serial
 
-# import modules
-
-import serial # serial communication
 from PIL import Image
 import numpy as np
 from scipy import ndimage
 import pygame
 import tkinter
 from tkinter import filedialog
-import math 
+import math
